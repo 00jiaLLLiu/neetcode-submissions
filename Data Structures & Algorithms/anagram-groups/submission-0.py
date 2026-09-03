a321c34a382dcb1f3098223d0427d18407ce8560
EvelynLiu <113569451+00jiaLLLiu@users.noreply.github.com>
2026-09-02 23:33:38 -0400

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,12 +1,8 @@
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res= defaultdict(list)
-        for s in strs:
-            sorteds = "".join(sorted(s))
-            res[sorteds].append(s)
-
-        return list(res.values())
-
-
-
+        res = defaultdict(list)
+        for str in strs:
+            sortS = "".join(sorted(str))
+            res[sortS].append(str)
+        return list(res.values()) 
+        #res.values(). 返回 dict_values([group1, group2, group3]) 不是list
